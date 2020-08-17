d0077443f0de4762943e7841110da03bc4bdbbcf
TGhidei <tghidei@rogers.com>
2020-08-16 20:24:01 -0400

    SpringBoot Initializer

---
+++ base64encoding/src/main/java/com/jenkins/tutorial/base64encoding/Base64encodingApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.tutorial.base64encoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Base64encodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Base64encodingApplication.class, args);
+	}
+
+}
---
+++ base64encoding/src/test/java/com/jenkins/tutorial/base64encoding/Base64encodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.tutorial.base64encoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base64encodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
